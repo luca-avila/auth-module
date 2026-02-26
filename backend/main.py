97b24bdbf3af2f5be1ecc19daa69ac05a26f1f0d
luca-avila <avilaluca61@gmail.com>
2026-02-25 21:26:57 -0300

    Improve app run settings

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,10 @@
 import uvicorn
+from config import settings
 
 if __name__ == "__main__":
-    uvicorn.run("api_app:app", host="0.0.0.0", log_level="info", reload=True)
+    uvicorn.run(
+        "api_app:app",
+        host="0.0.0.0",
+        log_level="info",
+        reload=settings.ENVIRONMENT == "development",
+    )
